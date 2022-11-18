d5f470e8957b70d4ea67dd6bbb350ca48aedca7e
Lars Norberg <github@larsnorbergofficial.com>
2022-11-18 14:57:56 +0100

    add mirror timers and timer trackers

---
+++ AzeriteUI/Components/Blizzard/Timers.lua
@@ -0,0 +1,171 @@
+--[[
+
+	The MIT License (MIT)
+
+	Copyright (c) 2022 Lars Norberg
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+--]]
+local Addon, ns = ...
+local Timers = ns:NewModule("Timers", "LibMoreEvents-1.0", "AceTimer-3.0", "AceHook-3.0")
+
+-- Lua API
+local _G = _G
+local unpack = unpack
+
+-- Addon API
+local SetObjectScale = ns.API.SetObjectScale
+
+-- Cache of handled elements
+local Handled = {}
+
+Timers.UpdateMirrorTimers = function(self)
+
+	local db = ns.Config.Timers
+
+	for i = 1, MIRRORTIMER_NUMTIMERS do
+		local timer  = _G["MirrorTimer"..i]
+		if (timer) then
+			local bar = _G[timer:GetName().."StatusBar"]
+			if (not Handled[bar]) then
+
+				SetObjectScale(timer)
+
+				if (i == 1) then
+					timer:ClearAllPoints()
+					timer:SetPoint(unpack(db.MirrorTimerPosition))
+				end
+
+				local oldborder = _G[timer:GetName().."Border"]
+				local label = _G[timer:GetName().."Text"]
+
+				for i = 1, bar:GetNumRegions() do
+					local region = select(i, bar:GetRegions())
+					if (region:GetObjectType() == "Texture") then
+						region:SetTexture(nil)
+					end
+				end
+				oldborder:SetTexture(nil)
+				timer:DisableDrawLayer("BACKGROUND")
+
+				bar:SetStatusBarTexture(db.MirrorTimerBarTexture)
+				bar:GetStatusBarTexture():SetDrawLayer("BORDER", 0)
+				bar:SetSize(unpack(db.MirrorTimerBarSize))
+				bar:ClearAllPoints()
+				bar:SetPoint(unpack(db.MirrorTimerBarPosition))
+
+				label:SetFontObject(db.MirrorTimerLabelFont)
+				label:SetTextColor(unpack(db.MirrorTimerLabelColor))
+				label:ClearAllPoints()
+				label:SetPoint(unpack(db.MirrorTimerLabelPosition))
+
+				local backdrop = bar:CreateTexture(nil, "BACKGROUND", nil, -5)
+				backdrop:SetPoint(unpack(db.MirrorTimerBackdropPosition))
+				backdrop:SetSize(unpack(db.MirrorTimerBackdropSize))
+				backdrop:SetTexture(db.MirrorTimerBackdropTexture)
+				backdrop:SetVertexColor(unpack(db.MirrorTimerBackdropColor))
+
+				Handled[bar] = true
+			end
+			bar:SetStatusBarColor(unpack(db.MirrorTimerBarColor))
+		end
+
+	end
+end
+
+Timers.UpdateTimerTrackers = function(self, event, ...)
+
+	local db = ns.Config.Timers
+
+	for _,timer in pairs(TimerTracker.timerList) do
+		local bar = timer and timer.bar
+		if (bar) then
+			if (not Handled[bar]) then
+
+				SetObjectScale(timer)
+
+				for i = 1, bar:GetNumRegions() do
+					local region = select(i, bar:GetRegions())
+					if (region:GetObjectType() == "Texture") then
+						region:SetTexture(nil)
+					elseif (region:GetObjectType() == "FontString") then
+						-- Should only be one, and it's the time, not the label.
+						region:SetFontObject(db.MirrorTimerLabelFont)
+						region:SetTextColor(unpack(db.MirrorTimerLabelColor))
+						region:ClearAllPoints()
+						region:SetPoint(unpack(db.MirrorTimerLabelPosition))
+					end
+				end
+
+				bar:SetStatusBarTexture(db.MirrorTimerBarTexture)
+				bar:GetStatusBarTexture():SetDrawLayer("BORDER", 0)
+				bar:DisableDrawLayer("BACKGROUND")
+				bar:SetSize(unpack(db.MirrorTimerBarSize))
+				bar:ClearAllPoints()
+				bar:SetPoint(unpack(db.MirrorTimerBarPosition))
+
+				local backdrop = bar:CreateTexture(nil, "BACKGROUND", nil, -5)
+				backdrop:SetPoint(unpack(db.MirrorTimerBackdropPosition))
+				backdrop:SetSize(unpack(db.MirrorTimerBackdropSize))
+				backdrop:SetTexture(db.MirrorTimerBackdropTexture)
+				backdrop:SetVertexColor(unpack(db.MirrorTimerBackdropColor))
+
+				Handled[bar] = true
+			end
+			bar:SetStatusBarColor(unpack(db.MirrorTimerBarColor))
+		end
+	end
+end
+
+Timers.UpdateAll = function(self)
+	self:UpdateMirrorTimers("ForceUpdate")
+	if (ns.IsRetail) then
+		self:UpdateTimerTrackers("ForceUpdate")
+	end
+end
+
+Timers.OnInitialize = function(self)
+	if (ns.IsRetail) then
+		return self:Disable()
+	end
+
+	-- Reset scripts and events
+	for i = 1, MIRRORTIMER_NUMTIMERS do
+		local timer  = _G["MirrorTimer"..i]
+		if (timer) then
+			timer:SetParent(UIParent)
+			timer:SetScript("OnEvent", MirrorTimerFrame_OnEvent)
+			timer:SetScript("OnUpdate", MirrorTimerFrame_OnUpdate)
+			MirrorTimerFrame_OnLoad(timer)
+		end
+	end
+
+	-- Update mirror timers (breath/fatigue)
+	self:SecureHook("MirrorTimer_Show", "UpdateMirrorTimers")
+
+	-- Update timer trackers (instance/bg countdowns)
+	if (ns.IsRetail) then
+		self:RegisterEvent("START_TIMER", "UpdateTimerTrackers")
+	end
+
+	-- Update all on world entering
+	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdateAll")
+
+end
