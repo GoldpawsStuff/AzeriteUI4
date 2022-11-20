dce222cf04904c7a43af06a81c9e453127337e83
Lars Norberg <github@larsnorbergofficial.com>
2022-11-20 13:24:33 +0100

    whitespace

--- AzeriteUI/Locale/enUS.lua
+++ AzeriteUI/Locale/enUS.lua
@@ -1,5 +1,5 @@
 local Addon = ...
 local L = LibStub("AceLocale-3.0"):NewLocale(Addon, "enUS", true)
-if (not L) then 
-	return 
-end 
+if (not L) then
+	return
+end
--- AzeriteUI/Locale/ruRU.lua
+++ AzeriteUI/Locale/ruRU.lua
@@ -1,5 +1,5 @@
 local Addon = ...
 local L = LibStub("AceLocale-3.0"):NewLocale(Addon, "ruRU")
-if (not L) then 
-	return 
-end 
+if (not L) then
+	return
+end
