3fab7ed13737d167c983aea1bddfe5651e8a9443
Lars Norberg <github@larsnorbergofficial.com>
2022-11-27 14:36:55 +0100

    fix wrath tracker position

--- AzeriteUI/Config/Blizzard/Tracker.lua
+++ AzeriteUI/Config/Blizzard/Tracker.lua
@@ -36,7 +36,7 @@ Config.Tracker = {
 	-- Size of the holder. Set to same width as our Minimap.
 	-- *Wrath WatchFrame is 306 expanded, 204 standard width, Retail ObjectiveTracker 248
 	Size = { 213, 22 },
-	Position = { "TOPRIGHT", UIParent, "TOPRIGHT", -60, -240 },
+	Position = { "TOPRIGHT", UIParent, "TOPRIGHT", ns.IsWrath and -90 or -60, -240 }, -- might need to adjust
 	BottomOffset = 380,
 	WrathScale = 1.0625,
 	WrathTitleFont = GetFont(12,true),
