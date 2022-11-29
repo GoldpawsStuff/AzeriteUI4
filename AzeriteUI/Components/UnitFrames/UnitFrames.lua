c4ca750f415a250095d815728f61cd3b636afabc
Lars Norberg <github@larsnorbergofficial.com>
2022-11-29 14:24:39 +0100

    properly disable prd classpower

--- AzeriteUI/Components/UnitFrames/UnitFrames.lua
+++ AzeriteUI/Components/UnitFrames/UnitFrames.lua
@@ -534,9 +534,31 @@ UnitFrames.KillNamePlateClutter = function(self)
 		return
 	end
 
-	local BlizzPlateManaBar = ClassNameplateManaBarFrame -- NamePlateDriverFrame.classNamePlatePowerBar
-	if (BlizzPlateManaBar) then
-		BlizzPlateManaBar:SetAlpha(0)
+	--local BlizzPlateManaBar = ClassNameplateManaBarFrame -- NamePlateDriverFrame.classNamePlatePowerBar
+	--if (BlizzPlateManaBar) then
+	--	BlizzPlateManaBar:SetAlpha(0)
+	--end
+
+	if (NamePlateDriverFrame.classNamePlatePowerBar) then
+		NamePlateDriverFrame.classNamePlatePowerBar:Hide()
+		NamePlateDriverFrame.classNamePlatePowerBar:UnregisterAllEvents()
+	end
+
+	if (NamePlateDriverFrame.SetupClassNameplateBars) then
+		hooksecurefunc(NamePlateDriverFrame, "SetupClassNameplateBars", function(nameplate)
+			if (not nameplate or nameplate:IsForbidden()) then
+				return
+			end
+
+			if (nameplate.classNamePlateMechanicFrame) then
+				nameplate.classNamePlateMechanicFrame:Hide()
+			end
+
+			if (nameplate.classNamePlatePowerBar) then
+				nameplate.classNamePlatePowerBar:Hide()
+				nameplate.classNamePlatePowerBar:UnregisterAllEvents()
+			end
+		end)
 	end
 
 	if (NamePlateDriverFrame.UpdateNamePlateOptions) then
